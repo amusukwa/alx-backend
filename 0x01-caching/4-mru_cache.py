26dee49fb50e5a80e942d8b510dfffe3697b8336
amusukwa <amusukwa@gmail.com>
2024-04-05 11:53:21 -0700

    4-mru_cache.py error

--- 0x01-caching/4-mru_cache.py
+++ 0x01-caching/4-mru_cache.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """ MRUCache module
 """
+from collections import deque
 from base_caching import BaseCaching
 
 
@@ -12,6 +13,7 @@ class MRUCache(BaseCaching):
         """ Initializes the MRUCache instance
         """
         super().__init__()
+        self.mru_queue = deque()
 
     def put(self, key, item):
         """ Add an item in the cache
@@ -20,12 +22,13 @@ class MRUCache(BaseCaching):
             if len(self.cache_data) >= self.MAX_ITEMS:
                 self.remove_most_recently_used()
             self.cache_data[key] = item
+            self.mru_queue.append(key)
 
     def remove_most_recently_used(self):
         """ Removes the most recently used item from the cache
         """
-        if self.cache_data:
-            mru_key = next(reversed(self.cache_data))
+        if self.mru_queue:
+            mru_key = self.mru_queue.pop()
             del self.cache_data[mru_key]
             print("DISCARD:", mru_key)
 
@@ -33,6 +36,8 @@ class MRUCache(BaseCaching):
         """ Get an item by key
         """
         if key is not None and key in self.cache_data:
-            self.cache_data.move_to_end(key)
+            # Move the accessed key to the end of the MRU queue
+            self.mru_queue.remove(key)
+            self.mru_queue.append(key)
             return self.cache_data[key]
         return None
